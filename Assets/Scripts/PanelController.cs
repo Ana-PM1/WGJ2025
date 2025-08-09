e7c48a9fac40f516cb42c1ad9ab0880175023e03
Ana-PM1 <akarenpm1@gmail.com>
2025-08-09 16:22:05 -0600

    menuInicioLayout

---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+
+    public GameObject panelCredits;
+   // public GameObject panelConfiguration;
+    public GameObject panelInstructions;
+
+    public void TogglePanelCredits()
+    {
+        panelCredits.SetActive(!panelCredits.activeSelf);
+    }
+
+    public void HidePanelCredits()
+    {
+        panelCredits.SetActive(false);
+    }
+
+   /* public void TogglePanelConfiguration()
+    {
+        panelConfiguration.SetActive(!panelConfiguration.activeSelf);
+    }
+
+    public void HidePanelConfiguration()
+    {
+        panelConfiguration.SetActive(false);
+    }
+   */
+
+    public void TogglePanelInstructions()
+    {
+        panelInstructions.SetActive(!panelInstructions.activeSelf);
+    }
+
+    public void HidePanelInstructions()
+    {
+        panelInstructions.SetActive(false);
+    }
+
+}
