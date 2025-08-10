1b238c8c9a520c474a552abce1cb1b9c0c57a4a7
Ana-PM1 <akarenpm1@gmail.com>
2025-08-10 10:09:47 -0600

    MenuUI

--- Assets/Script/PanelController.cs
+++ Assets/Script/PanelController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PanelController : MonoBehaviour
 {
@@ -8,6 +9,11 @@ public class PanelController : MonoBehaviour
     public GameObject panelCredits;
    // public GameObject panelConfiguration;
     public GameObject panelInstructions;
+
+    public void IniciarJuego()
+    {
+        SceneManager.LoadSceneAsync("Intro");
+    }
 
     public void TogglePanelCredits()
     {
